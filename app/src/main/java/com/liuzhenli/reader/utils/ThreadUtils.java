1d1fd1c7e1f03f1e4e7c90cac073cd6361b891a2
liuzhl <liuzhl@chineseall.com>
2021-02-24 16:46:45 +0800

    nothing

--- app/src/main/java/com/liuzhenli/reader/utils/ThreadUtils.java
+++ app/src/main/java/com/liuzhenli/reader/utils/ThreadUtils.java
@@ -1,8 +1,6 @@
 package com.liuzhenli.reader.utils;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
