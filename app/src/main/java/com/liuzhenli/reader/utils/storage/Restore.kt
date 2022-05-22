9e39a2f6d5091a906ec0e88b581146dcbd407ba9
liuzhl <liuzhl@chineseall.com>
2022-05-22 20:43:22 +0800

    fix: restore from backups

--- app/src/main/java/com/liuzhenli/reader/utils/storage/Restore.kt
+++ app/src/main/java/com/liuzhenli/reader/utils/storage/Restore.kt
@@ -6,11 +6,13 @@ import android.text.TextUtils
 import androidx.documentfile.provider.DocumentFile
 import com.liuzhenli.common.BaseApplication
 import com.liuzhenli.common.FileHelp
+import com.liuzhenli.common.FileHelp.createFileIfNotExist
 import com.liuzhenli.common.SharedPreferencesUtil
-import com.liuzhenli.common.utils.DocumentUtil
-import com.liuzhenli.common.utils.GsonUtil
-import com.liuzhenli.common.utils.LauncherIcon
-import com.liuzhenli.common.utils.fromJsonArray
+import com.liuzhenli.common.utils.*
+import com.liuzhenli.common.utils.filepicker.util.FileUtils.readText
+import com.liuzhenli.reader.ReaderApplication
+import com.liuzhenli.reader.utils.BackupRestoreUi
+import com.liuzhenli.reader.utils.isContentPath
 import com.micoredu.reader.R
 import com.micoredu.reader.bean.*
 import com.micoredu.reader.helper.AppReaderDbHelper
@@ -24,6 +26,7 @@ import io.reactivex.SingleOnSubscribe
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 import java.io.File
 
 object Restore {
@@ -34,48 +37,69 @@ object Restore {
                 for (fileName in Backup.backupFileNames) {
                     if (doc.name == fileName) {
                         DocumentUtil.readBytes(context, doc.uri)?.let {
-                            FileHelp.createFileIfNotExist(Backup.backupPath + File.separator + fileName)
-                                    .writeBytes(it)
+                            createFileIfNotExist(Backup.backupPath + File.separator + fileName)
+                                .writeBytes(it)
                         }
                     }
                 }
             }
             e.onSuccess(true)
         }).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : SingleObserver<Boolean> {
-                    override fun onSuccess(t: Boolean) {
-                        restore(Backup.backupPath, callBack)
-                    }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : SingleObserver<Boolean> {
+                override fun onSuccess(t: Boolean) {
+                    restore(Backup.backupPath, callBack)
+                }
 
-                    override fun onError(e: Throwable) {
-                        e.printStackTrace()
-                        callBack?.restoreError(e.localizedMessage ?: "ERROR")
-                    }
+                override fun onError(e: Throwable) {
+                    e.printStackTrace()
+                    callBack?.restoreError(e.localizedMessage ?: "ERROR")
+                }
 
-                    override fun onSubscribe(d: Disposable) {
-                    }
-                })
+                override fun onSubscribe(d: Disposable) {
+                }
+            })
+    }
+
+    fun getFilePath(path: String, fileName: String): String {
+        if (path.isContentPath()) {
+            return "${Backup.backupPath}${File.separator}$fileName"
+        }
+        return path + File.separator + "myBookShelf.json"
     }
 
     fun restore(path: String, callBack: Restore.CallBack?) {
         Single.create(SingleOnSubscribe<Boolean> { e ->
+            if (path.isContentPath()) {
+                DocumentFile.fromTreeUri(ReaderApplication.getInstance(), Uri.parse(path))
+                    ?.listFiles()?.forEach { doc ->
+                        for (fileName in Backup.backupFileNames) {
+                            if (doc.name == fileName) {
+                                readText(doc.uri.toString()).let {
+                                    createFileIfNotExist("${Backup.backupPath}${File.separator}$fileName")
+                                        .writeText(it)
+                                }
+                            }
+                        }
+                    }
+            }
             try {
-                val file = FileHelp.createFileIfNotExist(path + File.separator + "myBookShelf.json")
+                val file = createFileIfNotExist(getFilePath(path, "myBookShelf.json"))
                 val json = file.readText()
                 GsonUtil.fromJsonArray<BookShelfBean>(json)?.forEach { bookshelf ->
-                    if (bookshelf.noteUrl != null) {
-                        AppReaderDbHelper.getInstance().database.bookShelfDao.insertOrReplace(bookshelf)
-                    }
-                    if (bookshelf.bookInfoBean.noteUrl != null) {
-                        AppReaderDbHelper.getInstance().database.bookInfoDao.insertOrReplace(bookshelf.bookInfoBean)
-                    }
+                    AppReaderDbHelper.getInstance().database.bookShelfDao.insertOrReplace(
+                        bookshelf
+                    )
+                    AppReaderDbHelper.getInstance().database.bookInfoDao.insertOrReplace(
+                        bookshelf.bookInfoBean
+                    )
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
             }
             try {
-                val file = FileHelp.createFileIfNotExist(path + File.separator + "myBookSource.json")
+                val file =
+                    createFileIfNotExist(getFilePath(path, "myBookSource.json"))
                 val json = file.readText()
                 GsonUtil.fromJsonArray<BookSourceBean>(json)?.let {
                     BookSourceManager.addBookSource(it)
@@ -84,16 +108,20 @@ object Restore {
                 e.printStackTrace()
             }
             try {
-                val file = FileHelp.createFileIfNotExist(path + File.separator + "myBookSearchHistory.json")
+                val file =
+                    createFileIfNotExist(getFilePath(path, "myBookSearchHistory.json"))
                 val json = file.readText()
                 GsonUtil.fromJsonArray<SearchHistoryBean>(json)?.let {
-                    AppReaderDbHelper.getInstance().database.searchHistoryDao.insertOrReplaceInTx(it)
+                    AppReaderDbHelper.getInstance().database.searchHistoryDao.insertOrReplaceInTx(
+                        it
+                    )
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
             }
             try {
-                val file = FileHelp.createFileIfNotExist(path + File.separator + "myReadHistory.json")
+                val file =
+                    createFileIfNotExist(getFilePath(path, "myReadHistory.json"))
                 val json = file.readText()
                 GsonUtil.fromJsonArray<ReadHistory>(json)?.let {
                     AppReaderDbHelper.getInstance().database.readHistoryDao.insertOrReplace(it)
@@ -102,7 +130,8 @@ object Restore {
                 e.printStackTrace()
             }
             try {
-                val file = FileHelp.createFileIfNotExist(path + File.separator + "myBookReplaceRule.json")
+                val file =
+                    createFileIfNotExist(getFilePath(path, "myBookReplaceRule.json"))
                 val json = file.readText()
                 GsonUtil.fromJsonArray<ReplaceRuleBean>(json)?.let {
                     ReplaceRuleManager.addDataS(it)
@@ -111,7 +140,8 @@ object Restore {
                 e.printStackTrace()
             }
             try {
-                val file = FileHelp.createFileIfNotExist(path + File.separator + "myTxtChapterRule.json")
+                val file =
+                    createFileIfNotExist(getFilePath(path, "myTxtChapterRule.json"))
                 val json = file.readText()
                 GsonUtil.fromJsonArray<TxtChapterRuleBean>(json)?.let {
                     TxtChapterRuleManager.save(it)
@@ -119,7 +149,7 @@ object Restore {
             } catch (e: Exception) {
                 e.printStackTrace()
             }
-            val file = FileHelp.createFileIfNotExist(path + File.separator + "mmvConfig.json")
+            val file = createFileIfNotExist(getFilePath(path, "mmvConfig.json"))
             val json = file.readText()
             GsonUtil.fromJsonArray<MMKVBean>(json)?.map {
                 val value = it.value;
@@ -133,14 +163,16 @@ object Restore {
                     }
                     TextUtils.equals(MMKVBean.Type.BOOL, it.type) -> {
                         try {
-                            SharedPreferencesUtil.getInstance().putBoolean(it.key, value.toBoolean())
+                            SharedPreferencesUtil.getInstance()
+                                .putBoolean(it.key, value.toBoolean())
                         } catch (e: Exception) {
                             e.printStackTrace()
                         }
                     }
                     TextUtils.equals(MMKVBean.Type.FLOAT, it.type) -> {
                         try {
-                            SharedPreferencesUtil.getInstance().putFloat(it.key, value.toFloat())
+                            SharedPreferencesUtil.getInstance()
+                                .putFloat(it.key, value.toFloat())
                         } catch (e: Exception) {
                             e.printStackTrace()
                         }
@@ -149,7 +181,8 @@ object Restore {
 
                     TextUtils.equals(MMKVBean.Type.DOUBLE, it.type) -> {
                         try {
-                            SharedPreferencesUtil.getInstance().putDouble(it.key, value.toDouble())
+                            SharedPreferencesUtil.getInstance()
+                                .putDouble(it.key, value.toDouble())
                         } catch (e: Exception) {
                             e.printStackTrace()
                         }
@@ -177,28 +210,31 @@ object Restore {
                     }
                 }
             }
-
-
             e.onSuccess(true)
         }).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : SingleObserver<Boolean> {
-                    override fun onSuccess(t: Boolean) {
-                        LauncherIcon.ChangeIcon(SharedPreferencesUtil.getInstance().getString("launcher_icon", BaseApplication.getInstance().getString(R.string.icon_main)))
-                        ReadConfigManager.getInstance().updateReaderSettings()
-                        BaseApplication.getInstance().upThemeStore()
-                        BaseApplication.getInstance().initNightTheme()
-                        callBack?.restoreSuccess()
-                    }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : SingleObserver<Boolean> {
+                override fun onSuccess(t: Boolean) {
+                    LauncherIcon.ChangeIcon(
+                        SharedPreferencesUtil.getInstance().getString(
+                            "launcher_icon",
+                            BaseApplication.getInstance().getString(R.string.icon_main)
+                        )
+                    )
+                    ReadConfigManager.getInstance().updateReaderSettings()
+                    BaseApplication.getInstance().upThemeStore()
+                    BaseApplication.getInstance().initNightTheme()
+                    callBack?.restoreSuccess()
+                }
 
-                    override fun onError(e: Throwable) {
-                        e.printStackTrace()
-                        callBack?.restoreError(e.localizedMessage ?: "ERROR")
-                    }
+                override fun onError(e: Throwable) {
+                    e.printStackTrace()
+                    callBack?.restoreError(e.localizedMessage ?: "ERROR")
+                }
 
-                    override fun onSubscribe(d: Disposable) {
-                    }
-                })
+                override fun onSubscribe(d: Disposable) {
+                }
+            })
     }
 
 
